28c73c98b6cce2add3abaaa5a126a9225d8ec99b
Russell <miller.time.baby@gmail.com>
2018-03-31 10:20:46 -0700

    add button to add source

--- src/main/scala/manager/SourcesTab.scala
+++ src/main/scala/manager/SourcesTab.scala
@@ -1,18 +1,54 @@
 package manager
 
-import scalafx.scene.control.Tab
+import models.Model
+import scalafx.Includes._
+import scalafx.application.Platform
+import scalafx.geometry.Insets
+import scalafx.scene.control._
 import scalafx.scene.layout.VBox
 
 object SourcesTab extends Tab {
 
-  def apply() = {
-
+  def apply(): Tab = {
     new Tab {
       text = "Sources"
       closable = false
       content = new VBox {
-        children = List()
+        padding = Insets(20)
+        children = List(
+          new Button {
+            id = "addSourceButton"
+            text = "Add Source"
+            onAction = handle {addSource}
+          }
+        )
       }
+    }
+  }
+
+  def addSource: Unit = {
+    val addSourceDialog = new Dialog[String]() {
+      title = "Add Source"
+      headerText = "Enter path for new source"
+    }
+
+    addSourceDialog.dialogPane().buttonTypes = Seq(ButtonType.OK, ButtonType.Cancel)
+
+    val sourcePath = new TextField
+    addSourceDialog.dialogPane().content = sourcePath
+
+    Platform.runLater(sourcePath.requestFocus())
+
+    addSourceDialog.resultConverter = (dialogButton) => {
+      if (dialogButton == ButtonType.OK)
+        sourcePath.text()
+      else
+        null
+    }
+
+    addSourceDialog.showAndWait() match {
+      case Some(s: String) => Model.addSource(s)
+      case _ =>
     }
   }
 }
