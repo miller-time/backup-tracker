11d8215726670aeaf43efed67e5e30aeeddc5faf
Russell <miller.time.baby@gmail.com>
2018-04-01 12:42:38 -0700

    read store file

--- src/main/scala/models/Store.scala
+++ src/main/scala/models/Store.scala
@@ -1,11 +1,20 @@
 package models
 
+import java.io.File
+
 import scala.collection.mutable
+import scala.io.Source
 
 object Store {
 
   type ModelValues = Either[Seq[BackupSource], Seq[BackupDestination]]
   type ChangeHandler = (ModelValues) => Unit
+
+  val json = readStoreFile()
+  json match {
+    case Some(s) => println(s)
+    case None => println("file not found")
+  }
 
   /*
    * internal list of change handlers that have been registered
@@ -47,5 +56,18 @@ object Store {
     _backupDestinations += BackupDestination(path)
 
     changeHandlers.foreach(handler => handler(Right(backupDestinations)))
+  }
+
+  def readStoreFile(): Option[String] = {
+    val storeFilePath = System.getProperty("user.home")
+
+    try {
+      val source = Source.fromFile(storeFilePath + File.separator + ".backup-tracker.json")
+      val contents = source.mkString
+      source.close()
+      Some(contents)
+    } catch {
+      case _: java.io.FileNotFoundException => None
+    }
   }
 }
